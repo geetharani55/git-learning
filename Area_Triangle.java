9679ebfd592297bb51ffc9cd437f0f871aa54e9d
geethrani55 <geethamvdlgs@gmail.com>
2023-06-13 10:43:19 +0530

    added

---
+++ Area_Triangle.java
@@ -0,0 +1,16 @@
+package triangle;
+
+public class Area_Triangle 
+{
+    public static void main(String[] kk)
+    {
+        int b,h,a;
+        b=3;
+        h=4;
+        a=b*h/2;
+ // java print
+        System.out.println("Area of Triangle is "+a);
+ 
+        
+    }
+}
